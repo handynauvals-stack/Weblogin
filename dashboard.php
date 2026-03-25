3cf782292955b1f5c2334fb70d5fd6070840b621
handynauvals-stack <handynauvals@student.ub.ac.id>
2026-03-25 20:16:44 +0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@
 session_start();
 
 if (!isset($_SESSION["login"]) || $_SESSION["login"] != "Successfully") {
-    header("Location: login.php");
+    header("Location: index.html");
     exit();
 }
 
