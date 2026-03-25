9f370dc26d62271f9edb30319065ac2df344504d
handynauvals-stack <handynauvals@student.ub.ac.id>
2026-03-25 20:15:42 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+if (isset($_SESSION["login"]) && $_SESSION["login"] == "Successfully") {
+    header("Location: dashboard.php");
+    exit();
+}
+
+$email_benar = "admin";
+$pass_benar = "123";
+$pesan = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    if ($email == $email_benar && $password == $pass_benar) {
+        $_SESSION["login"] = "Successfully";
+        $_SESSION["email"] = $email;
+
+        header("Location: dashboard.php");
+        exit();
+    } else {
+        $pesan = "Username atau password salah.";
+    }
+}
+
+include "index.html";
+?>
