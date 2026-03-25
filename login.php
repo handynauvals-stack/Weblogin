27d8f4dca688bbe468ee2cf8a79558a375d33ee3
handynauvals-stack <handynauvals@student.ub.ac.id>
2026-03-25 20:14:27 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,41 +1,10 @@
 <!DOCTYPE html>
-<?php
-session_start();
-
-if (isset($_SESSION["login"]) && $_SESSION["login"] == "Successfully") {
-    header("Location: dashboard.php");
-    exit();
-}
-
-
-$email_benar = "admin";
-$pass_benar = "123";
-$pesan="";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    if ($email == $email_benar && $password == $pass_benar) {
-        $_SESSION["login"] = "Successfully";
-        $_SESSION["email"] = $email;
-
-        header("Location: dashboard.php");
-        exit();
-    } else {
-        $pesan = "Username atau password salah.";
-    }
-
-}
-?>
-
 <html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Login</title>
-
     <link rel="stylesheet" href="ui_login.css"/>
-
 </head>
 <body>
     <div class="Frame">
@@ -46,13 +15,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <form method="post">
                     <table border="1" class="loginbox">
                         <tr>
-                            <td colspan="1" >Email:</td>
+                            <td colspan="1">Email:</td>
                             <td colspan="2" id="inputbox">
-                                <input type="text" name="email" id="email"placeholder="email or username">
+                                <input type="text" name="email" id="email" placeholder="email or username">
                             </td>
                         </tr>
                         <tr>
-                            <td colspan="1" >Password:</td>
+                            <td colspan="1">Password:</td>
                             <td colspan="2" id="inputbox">
                                 <input type="password" name="password" id="password" placeholder="password">
                             </td>
@@ -60,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <tr colspan="2">
                             <td colspan="1"></td>
                             <td>
-                                <button type="submit" class="SignIn" >Log In</button>
+                                <button type="submit" class="SignIn">Log In</button>
                             </td>
                         </tr>
                     </table>
@@ -75,20 +44,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         </table>
                     </div>
                     <div class="message">
-                        <?php
-                            echo $pesan;
-                        ?>
+                        <?php echo $pesan; ?>
                     </div>
                 </form>
             </center>
         </section>
-        
-        
-        <script>
-            
-            
-        </script>
-
     </div>
 </body>
 </html>
